f33675273caf037d972c67e13876dfc665d3462f
Johan Enell <johan.enell@qlik.com>
2023-10-05 14:56:19 +0200

    ci: added test package (#12)

---
+++ src/pic-definition.js
@@ -0,0 +1,9 @@
+export default function definition({
+  layout, // eslint-disable-line no-unused-vars
+  context, // eslint-disable-line no-unused-vars
+}) {
+  return {
+    scales: {},
+    components: [],
+  };
+}
