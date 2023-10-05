c963710c19dfce79fcdd73c3be14e8e7511317e1
Johan Enell <johan.enell@qlik.com>
2023-10-05 14:50:22 +0200

    ci: added test package

---
+++ src/index.js
@@ -0,0 +1,91 @@
+import {
+  useElement,
+  useState,
+  useStaleLayout,
+  useSelections,
+  useRect,
+  useEffect,
+  useConstraints,
+} from '@nebula.js/stardust';
+import picassojs from 'picasso.js';
+import picassoQ from 'picasso-plugin-q';
+
+import properties from './object-properties';
+import data from './data';
+import picSelections from './pic-selections';
+import definition from './pic-definition';
+import ext from './ext';
+
+/**
+ * Entrypoint for your sense visualization
+ * @param {object} galaxy Contains global settings from the environment.
+ * Useful for cases when stardust hooks are unavailable (ie: outside the component function)
+ * @param {object} galaxy.anything Extra environment dependent options
+ * @param {object=} galaxy.anything.sense Optional object only present within Sense,
+ * see: https://qlik.dev/extend/build-extension/in-qlik-sense
+ */
+export default function supernova(galaxy) {
+  const picasso = picassojs();
+  picasso.use(picassoQ);
+
+  return {
+    qae: {
+      properties,
+      data,
+    },
+    ext: ext(galaxy),
+    component() {
+      const element = useElement();
+      const selections = useSelections();
+      const layout = useStaleLayout();
+      const rect = useRect();
+      const constraints = useConstraints();
+
+      const [instance, setInstance] = useState();
+
+      useEffect(() => {
+        const p = picasso.chart({
+          element,
+          data: [],
+          settings: {},
+        });
+
+        const s = picSelections({
+          selections,
+          brush: p.brush('selection'),
+          picassoQ,
+        });
+
+        setInstance(p);
+
+        return () => {
+          s.release();
+          p.destroy();
+        };
+      }, []);
+
+      useEffect(() => {
+        if (!instance) {
+          return;
+        }
+        instance.update({
+          data: [
+            {
+              type: 'q',
+              key: 'qHyperCube',
+              data: layout.qHyperCube,
+            },
+          ],
+          settings: definition({ layout, constraints }),
+        });
+      }, [layout, instance]);
+
+      useEffect(() => {
+        if (!instance) {
+          return;
+        }
+        instance.update();
+      }, [rect.width, rect.height, instance]);
+    },
+  };
+}
---
+++ test/integration/test.e2e.js
@@ -0,0 +1,9 @@
+/* eslint no-unused-vars: 0 */
+const { test, expect } = require('@playwright/test');
+
+test.describe('minimal', () => {
+  test('should test picasso chart', async ({ page }) => {
+    // add your e2e tests here...
+    // expect(1 + 1).toBe(2)
+  });
+});
