6fa2537877e0602ca0d3779e1e7502f0d68ba2d0
Niek van Staveren <niek.vanstaveren@qlik.com>
2023-10-26 14:58:04 +0200

    chore: check monorepo

--- actions/release-it/.release-it.js
+++ actions/release-it/.release-it.js
@@ -1,8 +1,17 @@
 const version = "${version}";
 const packageName = process.env.npm_package_name;
+const monorepo = process.env.monorepo;
 const scope = packageName.split("/")[1];
 
 const releaseBranches = ["main", "master", "release/*"];
+
+const checkRelease = (branchName) => {
+  console.log(branchName);
+  if (monorepo) {
+    console.log("This is monorepo", branchName);
+  }
+  return true;
+};
 
 module.exports = {
   plugins: {
@@ -16,7 +25,7 @@ module.exports = {
     },
   },
   git: {
-    push: true,
+    push: checkRelease(branchName),
     tagName: `${packageName}-v${version}`,
     commitsPath: ".",
     commitMessage: `chore(${scope}): released version v${version} [no ci]`,
