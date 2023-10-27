a8d7c4fea8fd9c36d895f9bcaa5fc516cb721c2d
Niek van Staveren <niek.vanstaveren@qlik.com>
2023-10-27 11:24:28 +0200

    chore: move monorepo check

--- actions/release-it/.release-it.js
+++ actions/release-it/.release-it.js
@@ -1,18 +1,10 @@
 const version = "${version}";
 const packageName = process.env.npm_package_name;
-const monorepo = process.env.monorepo;
-const branch_name = process.env.branch_name;
 const scope = packageName.split("/")[1];
 
 const releaseBranches = ["main", "master", "release/**"];
 
 // Check if branch conditions are met
-if (monorepo) {
-  if (branch_name.indexOf("release") > -1 && branch_name.indexOf(scope) < 0) {
-    console.log(`Skipping release of ${scope} since it should not be released from branch ${branch_name}`);
-    process.exit(0);
-  }
-}
 
 module.exports = {
   plugins: {
--- actions/release-it/check-version.js
+++ actions/release-it/check-version.js
@@ -3,5 +3,20 @@ const fs = require("fs");
 const jsonString = fs.readFileSync("./package.json");
 const packageJson = JSON.parse(jsonString);
 
-console.log("version from package.json", packageJson.version);
-console.log("branch name", process.env.branch_name);
+const branch = process.env.branch_name;
+const newVersion = packageJson.version;
+const { monorepo } = process.env;
+const packageName = process.env.npm_package_name;
+const scope = packageName.split("/")[1];
+
+if (monorepo) {
+  if (branch.indexOf("release") > -1 && branch.indexOf(scope) < 0) {
+    console.log(`Skipping release of ${scope} since it should not be released from branch ${branch}`);
+    process.exit(0);
+  }
+}
+
+if (branch.slice(-5, -2) !== newVersion.slice(0, 3)) {
+  console.log(`You are releasing from a patch release track. Release ${newVersion} is not allowed on branch ${branch}`);
+  process.exit(0);
+}
