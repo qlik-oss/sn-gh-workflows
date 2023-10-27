2a42aae81b9888aca821f28e3d4bc3a4e9f17285
Niek van Staveren <niek.vanstaveren@qlik.com>
2023-10-27 11:14:54 +0200

    chore: check branch name

--- actions/release-it/check-version.js
+++ actions/release-it/check-version.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
-console.log("in the check version", process.env.version);
 const jsonString = fs.readFileSync("./package.json");
 const packageJson = JSON.parse(jsonString);
 
 console.log("version from package.json", packageJson.version);
+console.log("branch name", process.env.branch_name);
