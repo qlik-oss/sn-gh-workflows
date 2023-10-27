44f27dec7e9ec03cfc280a61da26b8f1595bb505
Niek van Staveren <niek.vanstaveren@qlik.com>
2023-10-27 11:51:08 +0200

    chore: fix output

--- actions/release-it/check-version.js
+++ actions/release-it/check-version.js
@@ -5,18 +5,16 @@ const packageJson = JSON.parse(jsonString);
 
 const branch = process.env.branch_name;
 const newVersion = packageJson.version;
-const { monorepo } = process.env;
+const { monorepo } = process.env || false;
 const packageName = process.env.npm_package_name;
 const scope = packageName.split("/")[1];
 
 if (monorepo) {
   if (branch.indexOf("release") > -1 && branch.indexOf(scope) < 0) {
     console.log(`Skipping release of ${scope} since it should not be released from branch ${branch}`);
-    process.exit(1);
   }
 }
 
 if (branch.slice(-5, -2) !== newVersion.slice(0, 3)) {
   console.log(`You are releasing from a patch release track. Release ${newVersion} is not allowed on branch ${branch}`);
-  process.exit(1);
 }
