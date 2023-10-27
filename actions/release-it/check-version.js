ed617f6838f776990b7abd0de1cfebc34c1dad68
Niek van Staveren <niek.vanstaveren@qlik.com>
2023-10-27 11:27:42 +0200

    chore: exit with error

--- actions/release-it/check-version.js
+++ actions/release-it/check-version.js
@@ -12,11 +12,11 @@ const scope = packageName.split("/")[1];
 if (monorepo) {
   if (branch.indexOf("release") > -1 && branch.indexOf(scope) < 0) {
     console.log(`Skipping release of ${scope} since it should not be released from branch ${branch}`);
-    process.exit(0);
+    process.exit(1);
   }
 }
 
 if (branch.slice(-5, -2) !== newVersion.slice(0, 3)) {
   console.log(`You are releasing from a patch release track. Release ${newVersion} is not allowed on branch ${branch}`);
-  process.exit(0);
+  process.exit(1);
 }
