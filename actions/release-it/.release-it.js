6e47fd7abcbe8acd6da26fab8ab5b8831101601d
Niek van Staveren <Niek.vanstaveren@qlik.com>
2024-01-11 08:38:16 +0100

    feat: use release-it (#18)
    * feat: release-it config
    
    * fix: monorepo
    
    * fix: use branch
    
    * fix: add shell
    
    * fix: input arguments
    
    * fix: use yarn workspaces
    
    * fix: run yarn
    
    * fix: credentials
    
    * fix: tokens as input
    
    * fix: run in workspaces
    
    * fix: require branches
    
    * chore: verbose output
    
    * chore: show status
    
    * chore: add git status
    
    * fix: use chore commit
    
    * fix: do not copy release config
    
    * chore: remove git status calls
    
    * chore: release branches
    
    * chore: parse string
    
    * chore: set release branches
    
    * feat: add plugin
    
    * Revert "feat: add plugin"
    
    This reverts commit 8dc724cc4450185f47d5271bfc9f661b64552b9d.
    
    * chore: check monorepo
    
    * chore: check branchName existance
    
    * chore: get branch name
    
    * fix: correct syntax
    
    * fix: block releasing from release track
    
    * fix: do not push
    
    * chore: use release branches
    
    * chore: patch release strategy
    
    * chore: testing
    
    * chore: check in git plugin
    
    * chore: try increment
    
    * chore: try node file with version
    
    * chore: remove release track check
    
    * chore: fix action path
    
    * chore: try path variable
    
    * chore: fix env variables
    
    * chore: check branch name
    
    * chore: move monorepo check
    
    * chore: exit with error
    
    * chore: fix output
    
    * chore: fix output
    
    * chore: exit in release it flow
    
    * chore: try bin bash
    
    * fix: bash script call
    
    * chore: exit 1
    
    * chore: combined setup
    
    * chore: fix if else
    
    * fix: if statement
    
    * chore: verbose output
    
    * fix: allow non release track
    
    * fix: tag names single repos
    
    * fix: release name
    
    * chore: debug message
    
    * chore: try if statement
    
    * fix: monorepo check
    
    * fix: version checks
    
    * fix: add alpha and beta as branches
    
    * chore: remove verbose output
    
    * fix: remove console statement
    
    * chore: install dependencies in action
    
    * chore: setup dependencies
    
    * fix: use correct package manager
    
    * chore: only pnpm for monorepo
    
    * chore: move installation
    
    * chore: add package in npx command
    
    * chore: update dependency
    
    * fix: recursive call
    
    * chore: pull latest
    
    * chore: sequential run
    
    * chore: concurrency 1
    
    * chore: workspace concurrency 1
    
    * chore: api compliance
    
    * chore: pnpm as default
    
    * chore: conditional spec command
    
    * fix: add inputs
    
    * fix: condition
    
    * fix: set v2

---
+++ actions/release-it/.release-it.js
@@ -0,0 +1,55 @@
+const version = "${version}";
+const packageName = process.env.npm_package_name;
+const packageManager = process.env.package_manager;
+const actionPath = process.env.action_path;
+const monorepo = process.env.monorepo;
+const specCommand = process.env.spec_command;
+const scope = packageName.split("/")[1];
+
+const releaseBranches = ["main", "master", "release/**", "alpha", "beta"];
+
+let tagName = `v${version}`;
+if (monorepo === true || monorepo === "true") {
+  tagName = `${packageName}-${tagName}`;
+}
+
+module.exports = {
+  plugins: {
+    "@release-it/conventional-changelog": {
+      path: ".",
+      infile: "CHANGELOG.md",
+      preset: "conventionalcommits",
+      gitRawCommitsOpts: {
+        path: ".",
+      },
+    },
+  },
+  git: {
+    push: true,
+    tagName,
+    commitsPath: ".",
+    commitMessage: `chore(${scope}): released version v${version} [no ci]`,
+    requireCommits: true,
+    requireCommitsFail: false,
+    requireBranch: releaseBranches,
+  },
+  npm: {
+    publish: false,
+    versionArgs: ["--workspaces false"],
+  },
+  github: {
+    release: true,
+    releaseName: tagName,
+  },
+  hooks: {
+    "before:git:release": [
+      `if ${monorepo}; then mvm-update; fi`,
+      `#!/bin/bash
+      if [ -n "$(node ${actionPath}/check-version)" ]; then exit 1; fi`,
+      `if ${specCommand}; then ${packageManager} spec && ${packageManager} build; fi`,
+      `if ${specCommand}; then ${actionPath}/api-compliance.sh ${version}; fi`,
+      "git add --all",
+    ],
+    "after:git:release": [`${packageManager} publish`],
+  },
+};
---
+++ actions/release-it/check-version.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+
+const jsonString = fs.readFileSync("./package.json");
+const packageJson = JSON.parse(jsonString);
+
+const branch = process.env.branch_name || "";
+const { version } = packageJson;
+const { monorepo } = process.env || false;
+const packageName = process.env.npm_package_name || "";
+const scope = packageName.split("/")[1];
+
+const versionRegex = /(\d+\.)?(\d+\.)?(x|\d+)$/;
+
+if (!version) {
+  console.log("Could not perform version checks because of missing version in package.json");
+}
+
+const branchVersion = branch.match(versionRegex);
+const packageVersion = version.match(versionRegex);
+
+if (monorepo) {
+  if (branch.indexOf("release") > -1 && branch.indexOf(scope) < 0) {
+    console.log(`Skipping release of ${scope} since it should not be released from branch ${branch}`);
+  }
+}
+
+if (branch.indexOf("release") > -1) {
+  if (branchVersion[1] !== packageVersion[1]) {
+    console.log(
+      `You are releasing from a patch release track. Releasing major release ${version} is not allowed on branch ${branch}`,
+    );
+  }
+  if (branchVersion[2] !== packageVersion[2]) {
+    console.log(
+      `You are releasing from a patch release track. Releasing minor release ${version} is not allowed on branch ${branch}`,
+    );
+  }
+}
