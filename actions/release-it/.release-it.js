74c480375135bd83b0038456865a0113be5a2420
Niek van Staveren <Niek.vanstaveren@qlik.com>
2024-01-16 10:18:09 +0100

    fix: do extra install for write permission (#29)

--- actions/release-it/.release-it.js
+++ actions/release-it/.release-it.js
@@ -46,7 +46,7 @@ module.exports = {
       `if ${monorepo}; then mvm-update; fi`,
       `#!/bin/bash
       if [ -n "$(node ${actionPath}/check-version)" ]; then exit 1; fi`,
-      `if ${specCommand}; then ${packageManager} spec && ${packageManager} build; fi`,
+      `if ${specCommand}; then ${packageManager} install && ${packageManager} run spec && ${packageManager} run build; fi`,
       `if ${specCommand}; then ${actionPath}/api-compliance.sh ${version}; fi`,
       "git add --all",
     ],
