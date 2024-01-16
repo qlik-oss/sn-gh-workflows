956d7914095417ede16301adae65611350607626
Niek van Staveren <Niek.vanstaveren@qlik.com>
2024-01-16 23:03:51 +0100

    fix: assets and git cleanup (#32)
    * fix: only add assets
    
    * chore: test workflow from branch
    
    * fix: allow reassignment
    
    * fix: clean workspace
    
    * fix: require clean working dir
    
    * chore: set assets
    
    * chore: clean workspace
    
    * fix: monorepo check
    
    * fix: remove monorepo package.json
    
    * chore: clean before publish
    
    * chore: install dependencies before
    
    * chore: cleanup
    
    * fix: not through npx
    
    * chore: add fallback
    
    * fix: get package name
    
    * fix: get package name
    
    * chore: conditionally add specs
    
    * fix: correct check
    
    * chore: check git status
    
    * chore: cleanup
    
    * chore: add mvm lock file
    
    * fix: mvm lock location
    
    * chore: set to v2

--- actions/release-it/.release-it.js
+++ actions/release-it/.release-it.js
@@ -1,10 +1,27 @@
 const version = "${version}";
-const packageName = process.env.npm_package_name;
+let packageName = process.env.PNPM_PACKAGE_NAME;
 const packageManager = process.env.package_manager;
 const actionPath = process.env.action_path;
 const monorepo = process.env.monorepo;
 const specCommand = process.env.spec_command;
+
+if (!packageName) {
+  const fs = require("fs");
+  const jsonString = fs.readFileSync("./package.json");
+  const package = JSON.parse(jsonString);
+  packageName = package.name;
+}
 const scope = packageName.split("/")[1];
+
+let assets = "package.json ";
+if (process.env.API_SPECIFICATION_PATH) {
+  assets += process.env.API_SPECIFICATION_PATH;
+} else if (specCommand === true || specCommand === "true") {
+  assets += "api-spec/spec.json";
+}
+if (monorepo) {
+  assets += " ../**/mvm.lock";
+}
 
 const releaseBranches = ["main", "master", "release/**", "alpha", "beta"];
 
@@ -43,13 +60,14 @@ module.exports = {
   },
   hooks: {
     "before:git:release": [
+      "git clean -df",
       `if ${monorepo}; then mvm-update; fi`,
       `#!/bin/bash
       if [ -n "$(node ${actionPath}/check-version)" ]; then exit 1; fi`,
-      `if ${specCommand}; then npm run spec && ${packageManager} run build; fi`,
+      `if ${specCommand}; then ${packageManager} run spec && ${packageManager} run build; fi`,
       `if ${specCommand}; then ${actionPath}/api-compliance.sh ${version}; fi`,
-      "git add --all",
+      `git add ${assets}`,
     ],
-    "after:git:release": [`${packageManager} publish`],
+    "after:git:release": ["git reset --hard", "git clean -df", `${packageManager} publish`],
   },
 };
