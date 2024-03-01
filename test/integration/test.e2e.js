f0410fd8d10eaa986ec47139b64ebbbf5ea3c717
Johan Enell <johan.enell@qlik.com>
2024-03-01 16:01:16 +0100

    chore: removed example app
    Signed-off-by: Johan Enell <johan.enell@qlik.com>

--- test/integration/test.e2e.js
+++
@@ -1,9 +0,0 @@
-/* eslint no-unused-vars: 0 */
-const { test, expect } = require("@playwright/test");
-
-test.describe("minimal", () => {
-  test("should test picasso chart", async ({ page }) => {
-    // add your e2e tests here...
-    // expect(1 + 1).toBe(2)
-  });
-});
