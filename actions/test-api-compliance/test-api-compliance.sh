0e269effac6c82d06fe7d671105d7a4baf98cbda
Johan Enell <johan.enell@qlik.com>
2024-03-05 10:58:26 +0100

    fix: api and version in monorepos (#51)
    Signed-off-by: Johan Enell <johan.enell@qlik.com>

--- actions/test-api-compliance/test-api-compliance.sh
+++ actions/test-api-compliance/test-api-compliance.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# set -x
+
+exit_on_error() {
+  echo $1
+  exit 1
+}
+
 SELF_PATH=$(readlink -f "${BASH_SOURCE[0]:-"$(command -v -- "$0")"}")
 SELF_DIR=$(dirname $SELF_PATH)
 
@@ -7,44 +14,43 @@ if [ -z "$PNPM_PACKAGE_NAME" ]; then
 fi
 
 echo "Check compliance for $PNPM_PACKAGE_NAME"
+searchstring="/"
 
-echo "::group::Calculate next version"
-tag=$(pnpm --filter "$PNPM_PACKAGE_NAME" exec release-it --config $SELF_DIR/.release-it.json --release-version)
-release_it_result=$?
-echo "::endgroup::"
-if [ $release_it_result != 0 ]; then
-  echo "::error::Failed to calculate next version"
-  exit 1
-fi
-echo "Next version is $tag"
+package=${PNPM_PACKAGE_NAME#*"/"}
 
-echo "::group::Find api key"
-key=$(echo "$API_KEY" | jq '."$PNPM_PACKAGE_NAME"' 2>/dev/null)
+echo "::group::<$PNPM_PACKAGE_NAME> - Find api key"
+key=$(echo "$API_KEY" | jq -r ".\"$package\"" 2>/dev/null)
 if [ "$?" != "0" ]; then
   # The API_KEY was not a json so will treat it a string instead
   key="$API_KEY"
 fi
 echo "::endgroup::"
 
-if [ -z "$key" ]; then
-  echo "::warning::No API_KEY for $PNPM_PACKAGE_NAME. Will skip compliance check"
+if [ -z "$key" ] || [ $key == "null" ]; then
+  echo "::notice::No API_KEY for $package. Will skip compliance check"
   exit 0
 fi
 
-echo "::group::Generate new spec"
-pnpm run spec
+
+echo "::group::<$PNPM_PACKAGE_NAME> - Calculate next version"
+tag=$(pnpm --filter "$PNPM_PACKAGE_NAME" exec release-it --config $SELF_DIR/../release-it/.release-it.js --no-github --no-npm --git.requireBranch= --release-version)
+release_it_result=$?
+echo "Next version is $tag"
+echo "::endgroup::"
+if [ $release_it_result != 0 ]; then
+  exit_on_error "::error::Failed to calculate next version"
+fi
+
+echo "::group::<$PNPM_PACKAGE_NAME> - Generate new spec"
+pnpm run spec || exit_on_error "::error::Failed to run the spec script in $(pwd)"
 
 # Test if api specification has changed
-if ((git status --porcelain));
-then
-  echo "::error::Your API specification has changed when running yarn spec. Make sure you commit your API spec changes, or remove unintended changes in the properties definition"
-  exit 1
-fi
+git status --porcelain || exit_on_error "::error::Your API specification has changed when running yarn spec. Make sure you commit your API spec changes, or remove unintended changes in the properties definition"
 
 echo "$(jq --arg tag $tag '.info.version = $tag' $API_SPECIFICATION_PATH)" > "$API_SPECIFICATION_PATH"
 echo "::endgroup::"
 
-echo "::group::Run api-compliance"
+echo "::group::<$PNPM_PACKAGE_NAME> - Run api-compliance"
 docker pull ghcr.io/qlik-download/api-compliance
 docker create -v /specs --name specs alpine:3.4 "/bin/true"
 docker cp "$API_SPECIFICATION_PATH" specs:/specs/properties.json
@@ -61,6 +67,7 @@ api_compliance_result=$?
 echo "::endgroup::"
 
 if [ $api_compliance_result != 0 ]; then
-  echo "::error::API failed compliance check"
-  exit 1
+  exit_on_error "::error::API failed compliance check"
 fi
+
+git restore "$API_SPECIFICATION_PATH"
