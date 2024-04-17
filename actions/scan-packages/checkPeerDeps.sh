982d3545a26d9077dab52031af2282ba53cfdcef
Johan Enell <johan.enell@qlik.com>
2024-04-17 15:50:10 +0200

    fix: scan all packages in a monorepo for stardust
    Signed-off-by: Johan Enell <johan.enell@qlik.com>

---
+++ actions/scan-packages/checkPeerDeps.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+echo "Scanning $(cat package.json | jq -r '.name') in folder $(pwd)"
+
+stardust=$(cat package.json | jq -r '.peerDependencies["@nebula.js/stardust"]')
+
+if [ "$stardust" != "null"  ]; then
+    if [[ "$stardust" != \>* ]]; then
+      echo "::error::Incorrect Nebula peer dependency semver check, please use the form >=X.Y.Z and not ^X.Y.Z"
+      exit 1;
+    else 
+       echo "Stardust dependency correctly specified"
+    fi
+else
+  echo "::notice::No stardust peer dependency. Skipping this package"
+fi
