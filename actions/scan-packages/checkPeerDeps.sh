f9370df5e167198710e85bea900b2d84bc23d1fc
Johan Enell <johan.enell@qlik.com>
2024-04-25 14:12:21 +0200

    fix: changed stardust version check (#72)
    Signed-off-by: Johan Enell <johan.enell@qlik.com>

--- actions/scan-packages/checkPeerDeps.sh
+++ actions/scan-packages/checkPeerDeps.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
+# set -x
 
-echo "Scanning $(cat package.json | jq -r '.name') in folder $(pwd)"
+echo "Scanning package.json in folder $(pwd)"
 
 stardust=$(cat package.json | jq -r '.peerDependencies["@nebula.js/stardust"]')
 
-if [ "$stardust" != "null"  ]; then
-    if [[ "$stardust" != \>* ]]; then
-      echo "::error::Incorrect Nebula peer dependency semver check, please use the form >=X.Y.Z and not ^X.Y.Z"
-      exit 1;
-    else 
-       echo "Stardust dependency correctly specified"
-    fi
-else
-  echo "::notice::No stardust peer dependency. Skipping this package"
+if [ "$stardust" == "null"  ]; then
+  echo "::notice::No stardust peer dependency. Skipping $(pwd)"
+  exit 0;
+fi
+
+pattern="^>"
+if [[ $stardust =~ $pattern ]]; then
+  echo "Stardust dependency correctly specified"
+  exit 0;
+else 
+  echo "::error::Incorrect nebula stardust peer dependency semver check, please use the form >=X.Y.Z and not ^X.Y.Z ($(pwd), $stardust)"
+  exit 1;
 fi
