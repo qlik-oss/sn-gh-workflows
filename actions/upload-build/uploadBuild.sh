18447d7a0d08daadc6993b5c9819e9231602589a
Niek van Staveren <niek.vanstaveren@qlik.com>
2024-05-02 20:42:31 +0200

    fix: echo statements

--- actions/upload-build/uploadBuild.sh
+++ actions/upload-build/uploadBuild.sh
@@ -3,6 +3,11 @@
 
 systemjs=$(cat package.json | jq -r '.systemjs')
 
+echo "PWD"
+echo $(pwd)
+echo "Systemjs"
+echo $systemjs
+
 if [ "$systemjs" == "null" ]; then
   echo "::notice::No systemjs build for this package. Skipping $(pwd)"
   exit 0
