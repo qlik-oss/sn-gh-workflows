1ac76e02e9b88c9462238a5b6238c93484afbf3d
Niek van Staveren <niek.vanstaveren@qlik.com>
2024-05-02 20:59:29 +0200

    fix: remove comments

--- actions/upload-build/uploadBuild.sh
+++ actions/upload-build/uploadBuild.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 # set -x
 
-echo "Uploading files in folder $(pwd)"
+echo "Uploading dev builds for folder $(pwd)"
 
 systemjs=$(cat package.json | jq -r '.systemjs')
-
-echo "PWD"
-echo $(pwd)
-echo "Systemjs"
-echo $systemjs
 
 if [ "$systemjs" == "null" ]; then
   echo "::notice::No systemjs build for this package. Skipping $(pwd)"
